659bd3f84b5cb5034801e21e9704d298a726b639
herbzhao <herbzhao@gmail.com>
2020-04-14 21:18:56 +0100

    get and post

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+import json
+
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Look ma, hot abcdedf!'
 
-@app.route('/what')
-def what_world():
-    return 'What!'
+@app.route('/api/test_get', methods=['GET'])
+@cross_origin()
+def test_get():
+    return jsonify({'message': 'Hello, I am your backend'})
 
+@app.route('/api/test_post', methods=['POST'])
+@cross_origin()
+def test_post():
+    if request.method == "POST":
+        response = request.get_json()
+        result = "Hello " + response['user_name'] 
+        return  result
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
---
+++ frontend/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button v-on:click="test_get">Fetch info from our backend</button>
+    <br>
+    <input v-model="user_name" placeholder="user_name">
+      <br>
+    <button v-on:click="test_post">Send your name to backend</button>
+    <div>{{backend_response}}</div>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      user_name:  '',
+      backend_response: '',
+    };
+  },
+  props: {
+    msg: String
+  },
+  mounted() {
+  },
+  methods: {
+    test_get: function() {
+      var baseURI = "/api/test_get";
+      var vm = this;
+      // const baseURI = 'http://localhost:80/api/hello'
+      axios
+        .get(baseURI)
+        .then(response => {
+          console.log(response);
+          vm.backend_response = response.data.message;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    test_post: function() {
+      var baseURI = "/api/test_post";
+      var vm = this;
+      // const baseURI = 'http://localhost:80/api/hello'
+      axios
+        .post(baseURI, {
+          user_name: vm.user_name
+        })
+        .then(response => {
+          console.log(response.data);
+          vm.backend_response = response.data
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
